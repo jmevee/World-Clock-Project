c17416042a514a9e662ea89afd73654e5730fca8
jmevee <jaimie.tv@gmail.com>
2023-05-20 00:40:47 -0700

    added javascript to project

--- src/index.js
+++ src/index.js
@@ -1,1 +1,35 @@
+function updateTime() {
+  //Los Angeles
+  let losAngelesElement = document.querySelector("#los-angeles");
+  let losAngelesDateElement = losAngelesElement.querySelector(".date");
+  let losAngelesTimeElement = losAngelesElement.querySelector(".time");
+  let losAngelesTime = moment().tz("America/Los_Angeles");
 
+  losAngelesDateElement.innerHTML = losAngelesTime.format("MMM Do, YYYY");
+  losAngelesTimeElement.innerHTML = losAngelesTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+
+  //Seoul
+  let seoulElement = document.querySelector("#seoul");
+  let seoulDateElement = seoulElement.querySelector(".date");
+  let seoulTimeElement = seoulElement.querySelector(".time");
+  let seoulTime = moment().tz("Asia/Seoul");
+
+  seoulDateElement.innerHTML = seoulTime.format("MMM Do, YYYY");
+  seoulTimeElement.innerHTML = seoulTime.format("h:mm:ss [<small>]A[</small>]");
+
+  //Toronto
+  let torontoElement = document.querySelector("#toronto");
+  let torontoDateElement = torontoElement.querySelector(".date");
+  let torontoTimeElement = torontoElement.querySelector(".time");
+  let torontoTime = moment().tz("America/Toronto");
+
+  torontoDateElement.innerHTML = torontoTime.format("MMM Do, YYYY");
+  torontoTimeElement.innerHTML = torontoTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+}
+
+updateTime();
+setInterval(updateTime, 1000);
